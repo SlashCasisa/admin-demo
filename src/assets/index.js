9ce9f282ce2861f4157acac16d3d1ccfee2c8fff
liangcx <836513735@qq.com>
2020-07-07 17:02:56 +0800

    edit elementreset

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
 import '@/assets/iconfont/iconfont.css'; //引入Icon图标
 import '@/assets/reset.css' //引入重置样式
-import '@/assets/common/elementReset.scss' //引入element重置样式
+import '@/assets/common/elementreset.scss' //引入element重置样式
 import '@/assets/common/pageTableList.scss' //引入页面表格重置样式
